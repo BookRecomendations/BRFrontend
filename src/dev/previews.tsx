30fe8d949438c20a915f0cf6c2dc10ee9ace9380
Dawid Kielkowski <dawid55a@gmail.com>
2024-03-29 20:28:43 +0100

    Fix in ReactBuddy

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
 import Books from "../pages/books/Books.tsx";
 import PageLayout from "../pages/PageLayout.tsx";
+import Book from "../components/Book/Book.tsx";
 
 const ComponentPreviews = () => {
     return (
@@ -12,6 +12,17 @@ const ComponentPreviews = () => {
             </ComponentPreview>
             <ComponentPreview path="/PageLayout">
                 <PageLayout/>
+            </ComponentPreview>
+            <ComponentPreview path="/Book">
+                <Book book={{
+                    id: 1,
+                    bookId: 2,
+                    title: 'Harry Potter',
+                    description: 'A book about a wizard',
+                    url: 'https://www.goodreads.com/book/show/3.Harry_Potter_and_the_Sorcerer_s_Stone',
+                    imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SY475_.jpg',
+                    rating: 4.5,
+                }}/>
             </ComponentPreview>
         </Previews>
     );
