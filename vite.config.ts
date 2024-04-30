3a03935ec0f1b3c6c8f559fada18015238974be6
Dawid Kiełkowski <dawid55a@gmail.com>
2024-04-30 16:47:24 +0200

    Added docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
     server: {
-        host: "127.0.0.1",
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8080",
+    },
+
+    build: {
+        rollupOptions: {
+            external: ["./src/dev/**"],
+        },
     },
 });
